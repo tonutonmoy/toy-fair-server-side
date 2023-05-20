7cab1aeb86fb2f1edc588c6f8023b2802047f232
tonutonmoy <www.tonutonmoy12@gmail.com>
2023-05-20 20:30:14 +0600

    delete single data done

--- index.js
+++ index.js
@@ -171,6 +171,21 @@ async function run() {
    
 
 
+    // delete a toy
+
+    
+    app.delete('/deleteAToy/:id',async(req,res)=>{
+
+        const  id = req.params.id;
+
+        
+
+       const  query = {_id: new ObjectId(id)};
+
+          const result=  await allToysCollation.deleteOne(query)
+
+          res.send(result)
+    })
 
 
 
